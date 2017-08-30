b898106747416a56c5bb98231deeb292700384db
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-08-30 23:52:30 +0200

    Restructuring models, Receipt model added.

--- cl_inn/pos/models.py
+++ cl_inn/pos/models.py
@@ -13,6 +13,27 @@ class Shop(models.Model):
     is_active = models.BooleanField(default=True)
 
 
+class Receipt(models.Model):
+    
+    # Helpers
+    paid_msg = 'Money cannot be negative!'
+
+    # Attributes 
+    name = models.CharField(
+        max_length=255,
+        validators=[custom_validators.GeneralCMSValidator.name_validator]
+    )
+    date = models.DateTimeField(auto_now=True)
+    total_amount = models.FloatField()
+    paid_amount = models.FloatField(validators=[MinLengthValidator(0, paid_msg)])
+    user = models.ForeignKey(
+        'users.User',
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    cashier = models.IntegerField(default=-1) # Can be updated in future.
+
+
 class Item(models.Model):
 
     # Helpers
@@ -26,19 +47,20 @@ class Item(models.Model):
         validators=[custom_validators.GeneralCMSValidator.name_validator]
     )
     price = models.FloatField(validators=[MinLengthValidator(0, price_msg)])
+    discount = models.FloatField(validators=[MinLengthValidator(0, price_msg)], default=0)
     stock_amount = models.IntegerField(
         validators=[MinLengthValidator(0, stock_amount_msg)]
     )
-    shop = models.ForeignKey(
+    Receipt = models.ForeignKey(
         'Shop',
         related_name='items',
         on_delete=models.CASCADE
     )
-    user = models.ForeignKey(
-        'users.User',
-        related_name='items',
-        on_delete=models.CASCADE
-    )
-    cashier = models.IntegerField(default=-1) # Can be updated in future.
+
+    # Methods 
+    @property
+    def total_price(self):
+        """ Returns the calculated total price after discount."""
+        return self.price - (self.discount*self.price)
 
 
--- cl_inn/pos/tests/test_models.py
+++ cl_inn/pos/tests/test_models.py
@@ -4,8 +4,17 @@ from django.core.urlresolvers import resolve
 from django.core.exceptions import ValidationError
 from unittest import skip
 from users import User
+from pos import Shop, Item
 
-class ShopTest(TestCase):
+class ItemTest(TestCase):
 
 	def setUp(self):
+		self.shop = Shop.objects.create('Big Shop')
+
+	def test_name_with_normal_chars(self):
+		""" Saves item instance with proper name."""
 		
+		# Setup test
+		name = 'Big new item!'
+		# Exercise test
+		# Assert test
